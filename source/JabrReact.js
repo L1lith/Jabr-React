e17fdc6727085795228eab004403f30939f71f12
L1lith <lily@lillith.pw>
2018-08-12 13:17:19 -0600

    work on jabrreact

--- source/JabrReact.js
+++ source/JabrReact.js
@@ -1,6 +1,7 @@
 const {Component} = require('react')
 const Jabr = require('jabr')
 const findInObject = require('../functions/findInObject')
+const PropTypes = require('prop-types')
 
 class JabrReact extends Component {
   constructor(props) {
@@ -16,17 +17,27 @@ class JabrReact extends Component {
     if (!props.hasOwnProperty('select')) {
       throw new Error("You must supply a Select Object")
     } else if (typeof select == 'object' && select !== null) {
-      const selections = findInObject(select, val => val === true, {onInvalid: ()=>{
+      this.selections = findInObject(select, val => val === true, {onInvalid: ()=>{
         throw new Error("Invalid Select Object")
       }})
       console.log({selections})
     } else {
       throw new Error("Invalid Select Object")
     }
+
+    this.componentId = ++totalComponents
+  }
+  getChildContext() {
+    return {jabrStore: this.store, jabrSelections: this.selections}
   }
   render() {
-
+    return React.Children.only(this.props.children)
   }
+}
+
+JabrReact.childContextTypes = {
+  jabrStore: PropTypes.object,
+  jabrSelections: PropTypes.array
 }
 
 module.exports = JabrReact
